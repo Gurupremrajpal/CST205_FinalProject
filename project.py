dba2c88134a622aceedc03685cf8d8f302032cfa
Guruprem Rajpal <gurupremrajpal@gmail.com>
2021-12-09 05:14:49 +0530

    Fourth commit

--- project.py
+++ project.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template, redirect, request, session, url_for, g
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
+import smtplib
+from pprint import pprint
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from datetime import datetime
 from PIL import Image
 
 app = Flask(__name__)
+
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'csumb-otter'
 app.secret_key = 'csumb-otter'
+
+
 
 
 
@@ -25,9 +30,7 @@ class User:
 
 
 users = []
-users.append(User(id=1, username='Anthony', password='password'))
-users.append(User(id=2, username='Becca', password='secret'))
-users.append(User(id=3, username='Guruprem', password='Rajpal'))
+users.append(User(id=1, username='admin', password='P@ssword'))
 
 
 
@@ -36,7 +39,7 @@ def before_request():
     g.user = None
 
     if 'user_id' in session:
-        user = [x for x in users if x.id == session['user_id']][0]
+        user = [x for x in users if x.id == session['user_id']]
         g.user = user
 
 
@@ -53,7 +56,6 @@ def login():
         if user and user.password == password:
             session['user_id'] = user.id
             return redirect(url_for('survey'))
-
         return redirect(url_for('/'))
 
     return render_template('login.html')
@@ -78,17 +80,22 @@ class plist(FlaskForm):
         'Zip Code:',
         validators=[DataRequired()]
     )
+    city = StringField(
+        'City:',
+        validators=[DataRequired()]
+    )
     
 database = []
 
 
-def store_detail(my_firstname, my_lastname, my_email, my_phone, my_zip):
+def store_detail(my_firstname, my_lastname, my_email, my_phone, my_zip, my_city):
     database.append(dict(
         fname= my_firstname,
         lname= my_lastname,
         email= my_email,
         phonenumber = my_phone,
         zipnumber =my_zip,
+        cityy=my_city,
         date = datetime.today()
     ))
 
@@ -100,10 +107,12 @@ def survey():
         return redirect(url_for('/'))
     
     if form.validate_on_submit():
-        store_detail(form.first_name.data, form.last_name.data, form.email_id.data, form.Pno.data, form.zipNo.data)
+        store_detail(form.first_name.data, form.last_name.data, form.email_id.data, form.Pno.data, form.zipNo.data, form.city.data)
         print(plist)
         return redirect('/data_base')
+    
     return render_template('index.html', form=form)
+
 
 
 
